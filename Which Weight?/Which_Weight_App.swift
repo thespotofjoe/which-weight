0373d9d21caae205994c01b9581568d65a6f9954
Joe Buchoff <joebuchoff@gmail.com>
2021-09-06 12:36:13 -0500

    Initial Commit

---
+++ Which Weight?/Which_Weight_App.swift
@@ -0,0 +1,17 @@
+//
+//  Which_Weight_App.swift
+//  Which Weight?
+//
+//  Created by Joe on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Which_Weight_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
